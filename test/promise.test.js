258b05d00fd68263b07ed18b62a5515a45e1d2e1
Matt Glover <matthewglover@me.com>
2016-10-29 16:20:50 +0100

    feat(compose): add compose helper function

--- test/promise.test.js
+++ test/promise.test.js
@@ -1,5 +1,13 @@
 import test from 'ava';
+import { compose } from '../promise';
 
-test('Check everything working', (t) => {
-  t.pass();
+const addOne = x => x + 1;
+const timesTwo = x => x * 2;
+
+test('compose combines two functions', (t) => {
+  t.is(compose(addOne, timesTwo)(10), 21);
+});
+
+test('compose applies functions right to left', (t) => {
+  t.is(compose(timesTwo, addOne)(10), 22);
 });
