add0b62ce6f1c3abf26420ce875c33a296ab49cf
Matt Glover <matthewglover@me.com>
2016-10-30 10:33:17 +0000

    feat(sync-callbacks): handle sync callbacks

--- promise.js
+++ promise.js
@@ -3,21 +3,39 @@ export const compose = (f, g) => x => f(g(x));
 
 export default class Promise {
   constructor(f) {
+    this.__pending = true;
     this.__resolvers = [];
     this.__rejectors = [];
     f(this.__resolve.bind(this), this.__reject.bind(this));
   }
 
   then(resolve, reject) {
+    return this.__pending
+      ? this.__thenPending(resolve, reject)
+      : this.__thenDone(resolve, reject);
+  }
+
+  __thenPending(resolve, reject) {
     this.__resolvers.push(resolve);
     this.__rejectors.push(reject);
   }
 
+  __thenDone(resolve, reject) {
+    if (this.__resolved) resolve(this.__value);
+    else reject(this.__error);
+  }
+
   __resolve(value) {
+    this.__pending = false;
+    this.__resolved = true;
+    this.__value = value;
     this.__resolvers.forEach(f => f(value));
   }
 
   __reject(error) {
+    this.__pending = false;
+    this.__resolved = false;
+    this.__error = error;
     this.__rejectors.forEach(f => f(error));
   }
 }
--- test/promise.test.js
+++ test/promise.test.js
@@ -16,6 +16,12 @@ const simpleResolvingPromise = x =>
 const simpleRejectingPromise = () =>
   new Promise((resolve, reject) => asyncErrorFn(reject));
 
+const simpleResolvedPromise = x =>
+  new Promise(resolve => resolve(x * 2));
+
+const simpleRejectedPromise = () =>
+    new Promise((resolve, reject) => reject(testError));
+
 test('compose combines two functions', (t) => {
   t.is(compose(addOne, timesTwo)(10), 21);
 });
@@ -29,7 +35,7 @@ test('Promise takes a function and returns a Promise', (t) => {
   t.true(p instanceof Promise);
 });
 
-test.cb('Promise.then takes a resolve function which receives resolved value', (t) => {
+test.cb('Promise.then takes a resolve handler which receives resolved value', (t) => {
   t.plan(1);
   const p = simpleResolvingPromise(10);
   p.then((x) => {
@@ -38,9 +44,27 @@ test.cb('Promise.then takes a resolve function which receives resolved value', (
   });
 });
 
-test.cb('Promise.then takes a reject function which receives rejected error', (t) => {
+test.cb('Promise.then takes a reject handler which receives rejected error', (t) => {
   t.plan(1);
-  const p = simpleRejectingPromise(10);
+  const p = simpleRejectingPromise();
+  p.then(undefined, (error) => {
+    t.is(error, testError);
+    t.end();
+  });
+});
+
+test.cb('Promise.then calls resolve handler when resolved value already received', (t) => {
+  t.plan(1);
+  const p = simpleResolvedPromise(10);
+  p.then((x) => {
+    t.is(x, 20);
+    t.end();
+  });
+});
+
+test.cb('Promise.then calls reject handler when rejected error already received', (t) => {
+  t.plan(1);
+  const p = simpleRejectedPromise();
   p.then(undefined, (error) => {
     t.is(error, testError);
     t.end();
